a586d81c3ec0d244e6923699889aabef571aad96
Viri Ortiz <viriortiz32@gmail.com>
2021-05-14 04:37:54 -0500

    updated files

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// Dependencies
-const path = require('path');
-require('dotenv').config();
 const express = require('express');
-const routes = require('./controllers/');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars')
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const bcrypt = require ('bcrypt')
-// Handlebars helpers
+const path = require('path');
+
 const helpers = require('./utils/helpers');
 
-// Initialize handlebars for the html templates
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
-// Initialize sessions
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3306;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
-    secret: process.env.DB_SESSION_SECRET,
-    cookie: {
+  secret: 'itsabigsecret',
+  cookie: {
         // Session will automatically expire in 10 minutes
         expires: 10 * 60 * 1000
   },
@@ -28,29 +28,19 @@ const sess = {
     db: sequelize
   }),
 };
-// Initialize the server
-const app = express();
-// Define the port for the server
-const PORT = process.env.PORT || 3001;
 
-// Give the server a path to the public directory for static files
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set handlebars as the template engine for the server
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Have Express parse JSON and string data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Tell the app to use Express Session for the session handling
-app.use(session(sess));
-
-// Give the server the path to the routes
 app.use(routes);
 
-//server is listening
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+  app.listen(PORT, () => console.log('Now listening'));
+});
